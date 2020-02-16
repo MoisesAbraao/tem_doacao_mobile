1ee891bdcb2a82b161393f8d443e19a9fdc7c302
bgildson <gildson13@hotmail.com>
2020-02-16 09:55:31 -0300

    added social auth repository with google sign in implementation and tests

---
+++ lib/repositories/social_auth_repository.dart
@@ -0,0 +1,45 @@
+import 'package:dartz/dartz.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../core/errors/failures.dart';
+
+abstract class SocialAuthRepository {
+  /// try to sign in with google,
+  /// when the user cancels the operation, the result will be `null`
+  Future<Either<Failure, String>> googleSignIn();
+  Future<Either<Failure, void>> googleSignOut();
+}
+
+class SocialAuthRepositoryImpl implements SocialAuthRepository {
+  final GoogleSignIn _googleSignIn;
+
+  SocialAuthRepositoryImpl(this._googleSignIn);
+
+  @override
+  Future<Either<Failure, String>> googleSignIn() async {
+    try {
+      final googleSignInAccount = await _googleSignIn.signIn();
+
+      // canceled by the user
+      if (googleSignInAccount == null)
+        return Right(null);
+
+      GoogleSignInAuthentication authentication = await googleSignInAccount.authentication;
+
+      return Right(authentication.accessToken);
+    } catch (error) {
+      return Left(Failure.from(error));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> googleSignOut() async {
+    try {
+      await _googleSignIn.signOut();
+
+      return Right(null);
+    } catch (error) {
+      return Left(Failure.from(error));
+    }
+  }
+}
---
+++ test/repositories/social_auth_repository_test.dart
@@ -0,0 +1,80 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tem_doacao_mobile/core/errors/failures.dart';
+import 'package:tem_doacao_mobile/repositories/social_auth_repository.dart';
+
+class MockGoogleSignIn extends Mock implements GoogleSignIn {}
+
+class MockGoogleSignInAuthentication extends Mock implements GoogleSignInAuthentication {}
+
+class MockGoogleSignInAccount extends Mock implements GoogleSignInAccount {}
+
+void main() {
+  GoogleSignIn googleSignIn;
+  GoogleSignInAccount googleSignInAccount;
+  GoogleSignInAuthentication googleSignInAuthentication;
+  SocialAuthRepository repository;
+  final googleAccessToken = 'google access token';
+  final exception = PlatformException(code: '10', message: 'Occur an error');
+  final failure = Failure.from(exception);
+
+  setUp(() {
+    googleSignIn = MockGoogleSignIn();
+    googleSignInAccount = MockGoogleSignInAccount();
+    googleSignInAuthentication = MockGoogleSignInAuthentication();
+    repository = SocialAuthRepositoryImpl(googleSignIn);
+  });
+
+  group('googleSignIn', () {
+    test('with success', () async {
+      when(googleSignIn.signIn()).thenAnswer((_) async => googleSignInAccount);
+      when(googleSignInAccount.authentication)
+        .thenAnswer((_) async => googleSignInAuthentication);
+      when(googleSignInAuthentication.accessToken).thenReturn(googleAccessToken);
+
+      final result = await repository.googleSignIn();
+
+      verify(googleSignIn.signIn());
+      verify(googleSignInAccount.authentication);
+      verify(googleSignInAuthentication.accessToken);
+      expect(result, Right(googleAccessToken));
+    });
+
+    test('with user cancellation', () async {
+      when(googleSignIn.signIn()).thenAnswer((_) async => null);
+
+      final result = await repository.googleSignIn();
+
+      expect(result, Right(null));
+    });
+
+    test('with failure', () async {
+      when(googleSignIn.signIn()).thenThrow(PlatformException(code: '10'));
+
+      final result = await repository.googleSignIn();
+
+      expect(result, Left(failure));
+    });
+  });
+
+  group('googleSignOut', () {
+    test('with success', () async {
+      when(repository.googleSignOut()).thenAnswer((_) async => null);
+
+      final result = await repository.googleSignOut();
+
+      expect(result, Right(null));
+    });
+
+    test('with failure', () async {
+      when(repository.googleSignOut()).thenThrow(exception);
+
+      final result = await repository.googleSignOut();
+
+      expect(result, Left(failure));
+    });
+  });
+}
