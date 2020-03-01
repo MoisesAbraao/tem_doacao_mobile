6f56d40437114e349ca8065db2f3057c87236c63
bgildson <gildson13@hotmail.com>
2020-02-29 21:18:35 -0300

    defined basic styles

---
+++ lib/core/styles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+// COLORS
+const Color kPrimaryColor = const Color(0xFF2C2C37);
+const Color kPrimaryLightColor = const Color(0xFF60606B);
+const Color kAccentColor = const Color(0xFFFCAF4C);
+const Color kAccentLightColor = const Color(0xFFFCDBAA);
+const Color kSuccessColor = const Color(0xFF52C41A);
+const Color kWarnColor = const Color(0xFFF34229);
+const Color kLightColor = const Color(0xFFFBFCFC);
+const Color kInactiveColor = const Color(0xFFB7B5B2);
+const Color kSocialGoogleColor = const Color(0xFFEA4335);
+
+const MaterialColor kGreyColor = const MaterialColor(
+  0xFFAAAAAA,
+  const <int, Color>{
+    50: const Color(0xFFF4F4F4),
+    100: const Color(0xFFE3E3E5),
+    200: const Color(0xFFD7D7D9),
+    300: const Color(0xFFC8C8CC),
+    400: const Color(0xFFBBBBBF),
+    500: const Color(0xFFADADB2),
+    600: const Color(0xFFA1A1A6),
+    700: const Color(0xFF939399),
+    800: const Color(0xFF87878C),
+    900: const Color(0xFF797980)
+  }
+);
+
+// TYPOGRAPHY
+const double kTextH1Size = 64;
+const double kTextH2Size = 48;
+const double kTextH3Size = 32;
+const double kTextH4Size = 24;
+const double kTextH5Size = 20;
+const double kTextH6Size = 16;
+const double kTextH7Size = 14;
+const double kTextH8Size = 12;
